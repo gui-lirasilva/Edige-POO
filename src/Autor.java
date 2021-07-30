ca40c1a8034a13ceb5e1be8b9c3ec3345327fa28
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-07-29 21:34:09 -0300

    Criando classe Autor com validações e classe livro

---
+++ src/Autor.java
@@ -0,0 +1,64 @@
+public class Autor {
+
+    private String email;
+    private String nome;
+    private String biografia;
+
+
+    public Autor(String email, String nome, String biografia){
+        setBiografia(biografia);
+        setEmail(email);
+        setNome(nome);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        String modeloEmail = "^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$";
+        boolean validaEmail = email.matches(modeloEmail);
+
+        if(!validaEmail){
+            throw new IllegalArgumentException("E-mail inválido!");
+        }
+
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        //Validação 1
+        if (nome.length() > 50){
+            throw new IllegalArgumentException("Tamanho do nome superior a 50 caracteres");
+        }
+        //Validação 2
+        if (nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome não pode ser nulo nem vazio");
+        }
+
+        this.nome = nome;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        //Validação 1
+        if (biografia.length() > 250){
+            throw new IllegalArgumentException("Tamanho da biografia superior a 250 caracteres");
+        }
+        //Validação 2
+        if (biografia == null || biografia.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome não pode ser nulo nem vazio");
+        }
+
+        this.biografia = biografia;
+    }
+
+
+}
---
+++ src/Livro.java
@@ -0,0 +1,75 @@
+public class Livro {
+
+    String titulo;
+    String isbn;
+    String resumo;
+    String sumario;
+    int numeroDePaginas;
+    Autor autor;
+    String categoria;
+    Double preco;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public void setSumario(String sumario) {
+        this.sumario = sumario;
+    }
+
+    public int getNumeroDePaginas() {
+        return numeroDePaginas;
+    }
+
+    public void setNumeroDePaginas(int numeroDePaginas) {
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
